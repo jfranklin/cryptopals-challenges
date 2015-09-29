9392a7538460443d896c5a2c414022562a3ac2c7
Jody Franklin <jodyfranklin@elder-n00b.org>
2015-09-29 00:33:29 -0400

    Move hex2base64 function location to external file
    hex2base64() now included by require from h2b.php

--- set_1/1_hex2base64/h2b.php
+++ set_1/1_hex2base64/h2b.php
@@ -1,13 +1,10 @@
 <?php
 
+require 'hex2base64.php';
+
 $input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 
 $target = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-
-function hex2base64($hex) {
-  $base64 = base64_encode(pack("H*", $hex));
-  return $base64;
-}
 
 $output = hex2base64($input);
 
--- set_1/1_hex2base64/hex2base64.php
+++ set_1/1_hex2base64/hex2base64.php
@@ -1,7 +1,7 @@
 <?php
 
 function hex2base64($hex) {
-  $base64 = base64_encode(pack(H*, $hex));
+  $base64 = base64_encode(pack('H*', $hex));
   return $base64;
 }
 
