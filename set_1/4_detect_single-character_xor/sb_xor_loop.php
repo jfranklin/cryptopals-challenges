beb12640c07930c6acf4c762b594f067d2de3dc0
Jody Franklin <jodyfranklin@elder-n00b.org>
2015-10-01 23:13:33 -0400

    Change top score selection
    Add additional arrays to store the score, and key from the best
    choice for each line in the input file, using the line number
    as the key for both.
    
    Score array is sorted, then top scoring lines output, decrypted,
    along with the line number, key (ord() and chr() value), and
    score.

--- set_1/4_detect_single-character_xor/sb_xor_loop.php
+++ set_1/4_detect_single-character_xor/sb_xor_loop.php
@@ -2,17 +2,26 @@
 
 require 'letterScore.php';
 
-$filename="4.txt";
+if ($argc > 1) {
+  $filename = $argv[1];
+} else {
+  $filename="4.txt";
+}
+
 $lines = array();
 $file = fopen($filename, "r");
 while(!feof($file)) {
 
     //read file line by line into a new array element
     $lines[] = trim(fgets($file, 4096));
+    //remove blank lines, suppress empty element left by editors
+    $lines = array_filter($lines);
 
 }
 fclose ($file);
 
+$linesKey = [];
+$linesScore = [];
 
 foreach ($lines as $lineNumber => $line) {
 
@@ -34,19 +43,31 @@ foreach ($lines as $lineNumber => $line) {
 
   $keyCount = 0;
   foreach ($scores as $key => $value) {
-    if ($value > 150) {
-      if ($keyCount < 1) {
-        $decrypt = $input ^ str_repeat(chr($key), $inputLength);
-
-        printf("#%'03u '%1c' (%'03u) : %s\n",$lineNumber,$key,$key,$decrypt);
-
-      } else {
-        break;
-      }
-
-      $keyCount++;
+    if ($keyCount < 1) {
+      $linesKey[$lineNumber] = $key;
+      $linesScore[$lineNumber] = $value;
+    } else {
+      break;
     }
+
+    $keyCount++;
   }
+}
+
+arsort($linesScore);
+
+$lineCount = 0;
+printf("LINE KEY KEY#  SCORE       OUTPUT\n");
+foreach ($linesScore as $lineNumber => $lineScore) {
+  //return the top scoring entries in the file
+  if ($lineCount < 5) {
+    $lineLength = strlen($lines[$lineNumber]);
+    $decrypt = hex2bin($lines[$lineNumber]) ^ str_repeat(chr($linesKey[$lineNumber]), $lineLength);
+//    printf("#%'03u '%1c' (%'03u) %4.4f : %s\n",$lineNumber, $linesKey[$lineNumber], $linesKey[$lineNumber], $linesScore[$lineNumber], $decrypt);
+    printf("#%1$'03u '%2$1c' (%2$'03u) %3$+09.3f : %4$1s\n",$lineNumber, $linesKey[$lineNumber], $linesScore[$lineNumber], $decrypt);
+  }
+
+  $lineCount++;
 }
 
 ?>
